220744ef624bc62b14e30e1da75542156a841c3c
Nick Salvemini <salvemini.nick@gmail.com>
2023-08-02 12:32:27 -0400

    first commit

---
+++ diceRoll.js
@@ -0,0 +1,22 @@
+let diceValue = 2;
+
+let diceImage = document.getElementById('diceImage')
+
+if (diceValue = 1) {
+    diceImage.src = '/diceImages/1.png'
+}
+else if (diceValue = 2) {
+    diceImage.src = '/diceImages/2.png'
+}
+else if (diceValue = 3) {
+    diceImage.src = '/diceImages/3.png'
+}
+else if (diceValue = 4) {
+    diceImage.src = '/diceImages/4.png'
+}
+else if (diceValue = 5) {
+    diceImage.src = '/diceImages/5.png'
+}
+else if (diceValue = 6) {
+    diceImage.src = '/diceImages/6.png'
+}
