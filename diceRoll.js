46f10f359c36a2e279ecd2d8b3b6c02e9d719de5
Nick Salvemini <salvemini.nick@gmail.com>
2023-08-10 17:32:30 -0400

    adjusted placeDice function to shut off any events which includes click mouseover and mouseleave ALSO had to add a function to manually remove highlights but there is probably a better way to do that

--- diceRoll.js
+++ diceRoll.js
@@ -1,6 +1,5 @@
 let diceValue = 1;
 
-// let diceImg = document.getElementById('diceImg1')
 let diceImg1 = $('#diceImg1')
 
 function changeDiceValue() {
@@ -45,31 +44,14 @@ function addHightlight(col, r1, r2, r3) {
     })
 }
 
-// function removeHighlight(col, r1, r2, r3) {
-//     $(col).on('mouseleave', function () {
-//         $(col).removeClass('hover');
-//         if ($(r3).hasClass('filled') == false & $(r3).children().length != 0) {
-//             $(r3).children('img').remove();
-//         } else if ($(r2).hasClass('filled') == false & $(r2).children().length != 0) {
-//             $(r2).children('img').remove();
-//         } else if ($(r1).hasClass('filled') == false & $(r1).children().length != 0) {
-//             $(r1).children('img').remove();
-//         }
-//     })
-// }
-
 function removeHighlight(col, r1, r2, r3) {
     $(col).on('mouseleave', function () {
         $(col).removeClass('hover');
         if ($(r3).hasClass('filled') == false & $(r3).children().length != 0) {
             $(r3).children('img').remove();
-        }
-
-        else if ($(r2).hasClass('filled') == false & $(r2).children().length != 0) {
+        } else if ($(r2).hasClass('filled') == false & $(r2).children().length != 0) {
             $(r2).children('img').remove();
-        }
-
-        else if ($(r1).hasClass('filled') == false & $(r1).children().length != 0) {
+        } else if ($(r1).hasClass('filled') == false & $(r1).children().length != 0) {
             $(r1).children('img').remove();
         }
     })
@@ -84,25 +66,37 @@ function hover() {
     removeHighlight('.p1c3', '#p1c3r1', '#p1c3r2', '#p1c3r3');
 }
 
+function removeHover() {
+    $('td').each(function () {
+        if ($(this).hasClass('hover')) {
+            $(this).removeClass('hover')
+        }
+    })
+}
+
 function placeDice(colArr) {
     $.each(colArr, function (col) {
         $(colArr[col]).on('click', function () {
             if ($(colArr[col][2]).hasClass('filled') == false) {
                 $(colArr[col][2]).children('img').remove();
                 $(colArr[col][2]).addClass('filled');
-                $(colArr[col][2]).append(`<img style="height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`)
+                $(colArr[col][2]).append(`<img style="height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`);
+                removeHover()
             } else if ($(colArr[col][1]).hasClass('filled') == false) {
                 $(colArr[col][1]).children('img').remove();
                 $(colArr[col][1]).addClass('filled');
-                $(colArr[col][1]).append(`<img style="height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`)
+                $(colArr[col][1]).append(`<img style="height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`);
+                removeHover()
             } else if ($(colArr[col][0]).hasClass('filled') == false) {
                 $(colArr[col][0]).children('img').remove();
                 $(colArr[col][0]).addClass('filled');
-                $(colArr[col][0]).append(`<img style="height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`)
+                $(colArr[col][0]).append(`<img style="height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`);
+                removeHover()
             }
 
             disableEnable();
             diceImg1.addClass('hideImg');
+            $('td').off()
         })
     })
 }
