05b24ada698ed0f82d7ab9bb415baa349eb6294f
Nick Salvemini <salvemini.nick@gmail.com>
2023-08-10 17:12:10 -0400

    'added function to place a dice and reset the dice image and roll button AND need to figure out how to freeze the board so it can't be further clicked or highlighted until the next roll completes

--- diceRoll.js
+++ diceRoll.js
@@ -32,80 +32,6 @@ function changeDiceFace() {
     diceImg1.attr('src', `/diceImages/${num}.png`);
 }
 
-// function hover() {
-//     const p1c1 = $('.p1c1');
-//     const p1c2 = $('.p1c2');
-//     const p1c3 = $('.p1c3');
-
-//     p1c1.on('mouseenter', function () {
-//         if ($('#p1c1r1').hasClass('filled') == false) {
-//             p1c1.addClass('hover');
-//             if ($('#p1c1r3').hasClass('filled') == false) {
-//                 $('#p1c1r3').append(`<img style="opacity:0.5; height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`)
-//             } else if ($('#p1c1r2').hasClass('filled') == false) {
-//                 $('#p1c1r2').append(`<img style="opacity:0.5; height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`)
-//             } else { $('#p1c1r1').append(`<img style="opacity:0.5; height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`) }
-//         }
-//     });
-//     p1c1.on('mouseleave', function () {
-//         p1c1.removeClass('hover')
-//         if ($('#p1c1r3').hasClass('filled') == false & $('#p1c1r3').children().length != 0) {
-//             $('#p1c1r3').children('img').remove();
-//         } else if ($('#p1c1r2').hasClass('filled') == false & $('#p1c1r2').children().length != 0) {
-//             $('#p1c1r2').children('img').remove();
-//         } else if ($('#p1c1r1').hasClass('filled') == false & $('#p1c1r1').children().length != 0) {
-//             $('#p1c1r1').children('img').remove();
-//         }
-
-//     });
-
-
-//     p1c2.on('mouseenter', function () {
-//         if ($('#p1c2r1').hasClass('filled') == false) {
-//             p1c2.addClass('hover');
-//             if ($('#p1c2r3').hasClass('filled') == false) {
-//                 $('#p1c2r3').append(`<img style="opacity:0.5; height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`)
-//             } else if ($('#p1c2r2').hasClass('filled') == false) {
-//                 $('#p1c2r2').append(`<img style="opacity:0.5; height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`)
-//             } else { $('#p1c2r1').append(`<img style="opacity:0.5; height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`) }
-//         }
-//     });
-//     p1c2.on('mouseleave', function () {
-//         p1c2.removeClass('hover')
-//         if ($('#p1c2r3').hasClass('filled') == false & $('#p1c2r3').children().length != 0) {
-//             $('#p1c2r3').children('img').remove();
-//         } else if ($('#p1c2r2').hasClass('filled') == false & $('#p1c2r2').children().length != 0) {
-//             $('#p1c2r2').children('img').remove();
-//         } else if ($('#p1c2r1').hasClass('filled') == false & $('#p1c2r1').children().length != 0) {
-//             $('#p1c2r1').children('img').remove();
-//         }
-
-//     });
-
-
-//     p1c3.on('mouseenter', function () {
-//         if ($('#p1c3r1').hasClass('filled') == false) {
-//             p1c3.addClass('hover');
-//             if ($('#p1c3r3').hasClass('filled') == false) {
-//                 $('#p1c3r3').append(`<img style="opacity:0.5; height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`)
-//             } else if ($('#p1c3r2').hasClass('filled') == false) {
-//                 $('#p1c3r2').append(`<img style="opacity:0.5; height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`)
-//             } else { $('#p1c3r1').append(`<img style="opacity:0.5; height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`) }
-//         }
-//     });
-//     p1c3.on('mouseleave', function () {
-//         p1c3.removeClass('hover')
-//         if ($('#p1c3r3').hasClass('filled') == false & $('#p1c3r3').children().length != 0) {
-//             $('#p1c3r3').children('img').remove();
-//         } else if ($('#p1c3r2').hasClass('filled') == false & $('#p1c3r2').children().length != 0) {
-//             $('#p1c3r2').children('img').remove();
-//         } else if ($('#p1c3r1').hasClass('filled') == false & $('#p1c3r1').children().length != 0) {
-//             $('#p1c3r1').children('img').remove();
-//         }
-
-//     });
-// }
-
 function addHightlight(col, r1, r2, r3) {
     $(col).on('mouseenter', function () {
         if ($(r1).hasClass('filled') == false) {
@@ -119,14 +45,31 @@ function addHightlight(col, r1, r2, r3) {
     })
 }
 
+// function removeHighlight(col, r1, r2, r3) {
+//     $(col).on('mouseleave', function () {
+//         $(col).removeClass('hover');
+//         if ($(r3).hasClass('filled') == false & $(r3).children().length != 0) {
+//             $(r3).children('img').remove();
+//         } else if ($(r2).hasClass('filled') == false & $(r2).children().length != 0) {
+//             $(r2).children('img').remove();
+//         } else if ($(r1).hasClass('filled') == false & $(r1).children().length != 0) {
+//             $(r1).children('img').remove();
+//         }
+//     })
+// }
+
 function removeHighlight(col, r1, r2, r3) {
     $(col).on('mouseleave', function () {
         $(col).removeClass('hover');
         if ($(r3).hasClass('filled') == false & $(r3).children().length != 0) {
             $(r3).children('img').remove();
-        } else if ($(r2).hasClass('filled') == false & $(r2).children().length != 0) {
+        }
+
+        else if ($(r2).hasClass('filled') == false & $(r2).children().length != 0) {
             $(r2).children('img').remove();
-        } else if ($(r1).hasClass('filled') == false & $(r1).children().length != 0) {
+        }
+
+        else if ($(r1).hasClass('filled') == false & $(r1).children().length != 0) {
             $(r1).children('img').remove();
         }
     })
@@ -141,6 +84,28 @@ function hover() {
     removeHighlight('.p1c3', '#p1c3r1', '#p1c3r2', '#p1c3r3');
 }
 
+function placeDice(colArr) {
+    $.each(colArr, function (col) {
+        $(colArr[col]).on('click', function () {
+            if ($(colArr[col][2]).hasClass('filled') == false) {
+                $(colArr[col][2]).children('img').remove();
+                $(colArr[col][2]).addClass('filled');
+                $(colArr[col][2]).append(`<img style="height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`)
+            } else if ($(colArr[col][1]).hasClass('filled') == false) {
+                $(colArr[col][1]).children('img').remove();
+                $(colArr[col][1]).addClass('filled');
+                $(colArr[col][1]).append(`<img style="height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`)
+            } else if ($(colArr[col][0]).hasClass('filled') == false) {
+                $(colArr[col][0]).children('img').remove();
+                $(colArr[col][0]).addClass('filled');
+                $(colArr[col][0]).append(`<img style="height:4rem; width:4rem" src="/diceImages/${diceValue}.png">`)
+            }
+
+            disableEnable();
+            diceImg1.addClass('hideImg');
+        })
+    })
+}
 
 $('.roll').on('click', function () {
     disableEnable();
@@ -155,6 +120,9 @@ $('.roll').on('click', function () {
     setTimeout(() => {
         changeDiceValue();
         hover();
+
+        const colArr = [$('.p1c1'), $('.p1c2'), $('.p1c3')]
+        placeDice(colArr)
 
 
     }, 2251)
